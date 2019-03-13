fe535b35961a445de9296217107f9e44c6ae51d4
hoangphongliver <htphongx4@gmail.com>
2019-03-13 17:30:13 +0700

    f

---
+++ JS/login.js
@@ -0,0 +1,8 @@
+function onClickbtn(){
+    document.getElementById("formdn").style.display = "block";
+ }
+ 
+ 
+ function onClickbtn1(){
+     document.getElementById("formdn").style.display = "none";
+  }
